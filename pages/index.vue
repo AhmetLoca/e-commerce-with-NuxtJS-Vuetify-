8215045402c70807e76fe8e68f1c9eb5671c891f
ahmet <locaahmet@gmail.com>
2022-11-03 20:20:43 +0300

    create components and content

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Nav />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  async created() {
+    this.products = await this.$content("products").fetch();
+    this.sale_items = await this.$content("products")
+      .where({ onSale: true })
+      .fetch();
+  },
+  data() {
+    return {
+      //key: value
+      products: null,
+      sale_items: null,
+    };
+  },
+};
+</script>
+
+<style></style>
